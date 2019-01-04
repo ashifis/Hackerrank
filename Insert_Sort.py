a02afe75312442b2066ffc8183b0844095a05007
ashifis <ashifdesignlab@gmail.com>
2019-01-04 09:56:36 +0530

    Initial commit

---
+++ Insert_Sort.py
@@ -0,0 +1,14 @@
+dum_arr = [1, 13, 15, 45, 7, 87, 87, 77, 54, 6, 9, 89, 0, 3]
+for i in range(0, len(dum_arr)):
+    print(i)
+    for j in range(i, -1, -1):
+
+        if j > 0:
+            print(j)
+            if dum_arr[j]<dum_arr[j-1]:
+                temp = dum_arr[j]
+                dum_arr[j] = dum_arr[j - 1]
+                dum_arr[j - 1] = temp
+    print("====")
+
+print(dum_arr)
